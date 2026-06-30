a69b62afff6da53cb849b565c60b674494f3a701
Leonela Cardozo <leonela.cardozo@anima.edu.uy>
2026-06-30 20:04:47 -0300

    defino rutas de productos

--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getProducts,
+  getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
@@ -9,16 +10,10 @@ import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-
 router.get("/", verifyToken, getProducts);
-
-
+router.get("/:id", verifyToken, getProductById);
 router.post("/", verifyToken, createProduct);
-
-
 router.patch("/:id", verifyToken, updateProduct);
-
-
 router.delete("/:id", verifyToken, deleteProduct);
 
 export default router;
